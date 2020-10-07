e29dae7db26b767f064ba8433310cf3e325a5893
Matias Pascual <matias33pascual@gmail.com>
2020-10-07 13:28:07 -0300

    corrigiendo errores de tipeo

--- Views/cine-alta.php
+++ Views/cine-alta.php
@@ -5,7 +5,7 @@
     <section id="Agregar Cine" class="mb-5">
         <div class="container">
             <h2 class="mb-4">Agregar cine<h2>
-            <form action=<?php echo FRONT_ROOT ?>Cine/Add" method="post" class="bg-light-alpha p-5">
+            <form action="<?php echo FRONT_ROOT ?>Cine/Add" method="post" class="bg-light-alpha p-5">
                 <div class="row">                         
                     <div class="col-lg-4">
                         <div class="form-group">
