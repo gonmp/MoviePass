d1ad0427ec8398a7749ddcfa8a753fcd6106ca68
Gonzalo Perelstein <gmperelstein@gmail.com>
2020-10-19 21:36:08 -0300

    [GP] Se agregó el formulario para buscar las películas por género y fecha

--- Views/user-movie-form.php
+++ Views/user-movie-form.php
@@ -1,16 +1,35 @@
 <?php
-    require_once('header.php');
+    require_once('nav.php');
 ?>
-
-<form action="" method="post">
-    <label for="date">Ingrese la fecha</label>
-    <input type="date" name="date" id="date" min="" max="">
-    <label for="category">Categoría</label>
-    in
-</form>
+<main class="py-5">
+     <section id="listado" class="mb-5">
+          <div class="container">
+               <h2 class="mb-4">Buscador de películas</h2>
+               <form action="<?php echo FRONT_ROOT ?>Student/Add" method="post" class="bg-light-alpha p-5">
+                    <div class="row">                         
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                <label for="date">Ingrese la fecha</label>
+                                <input type="date" name="date" id="date" min="" max="">
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                    <label for="category">Categoría</label>
+                                    
+                              </div>
+                         </div>
+                         <div class="col-lg-4">
+                              <div class="form-group">
+                                   <label for="">Apellido</label>
+                                   <input type="text" name="lastName" value="" class="form-control">
+                              </div>
+                         </div>
+                    </div>
+                    <button type="submit" name="button" class="btn btn-dark ml-auto d-block">Agregar</button>
+               </form>
+          </div>
+     </section>
+</main>
 
 <script src="<?php echo JS_PATH ?>date.js"></script>
-
-<?php
-    require_once('footer.php');
-?>
