3bb310bc1ec0db98e043239306a5cc5688ec079b
Gonzalo Perelstein <gmperelstein@gmail.com>
2020-10-20 17:18:11 -0300

    [movie_controller_views] GP:
    -Se modificó el formulario para buscar películas para quitar el campo de la fecha

--- Views/user-movie-form.php
+++ Views/user-movie-form.php
@@ -7,12 +7,7 @@
                <h2 class="mb-4">Buscador de películas</h2>
                <form action="<?php echo FRONT_ROOT ?>Movie/ShowResultMovieView" method="post" class="bg-light-alpha p-5">
                     <div class="row">                         
-                         <div class="col-lg-4">
-                              <div class="form-group">
-                                <label for="date">Ingrese la fecha</label>
-                                <input type="date" name="date" id="date" min="" max="">
-                              </div>
-                         </div>
+                         
                          <div class="col-lg-4">
                               <div class="form-group">
                                    <label for="category">Categoría</label>
