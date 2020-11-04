7a2aa6d006e58bba7b8fe52e47490554c5870a33
Gonzalo Perelstein <gmperelstein@gmail.com>
2020-11-03 23:13:33 -0300

    [Entrega_2] GP: Se corrigió el scrip de la base de datos para incluir el sistema de almacenamiento InnoDb

--- Scripts/database.sql
+++ Scripts/database.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS MoviePass;
 
 USE MoviePass;
 
-CREATE TABLE IF NOT EXISTS Movies (
+CREATE TABLE IF NOT EXISTS movies (
     id int,
     title varchar(30),
     popularity float,
@@ -16,32 +16,31 @@ CREATE TABLE IF NOT EXISTS Movies (
     vote_average float,
     overview varchar(100),
     CONSTRAINT pk_movies PRIMARY KEY (id)
-);
+)Engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS Genres(
+CREATE TABLE IF NOT EXISTS genres(
     id int,
-    name varchar(30),
+    name varchar(30) NOT NULL,
     CONSTRAINT pk_genres PRIMARY KEY (id)
-);
+)Engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS MoviesGenres(
-    movieId int,
-    genreId int,
-    CONSTRAINT pk_moviesgenres PRIMARY KEY (movieId, genreId),
-    CONSTRAINT fk_movies FOREIGN KEY (movieId) REFERENCES Movies(id) ON DELETE CASCADE,
-    CONSTRAINT fk_genres FOREIGN KEY (genreId) REFERENCES Genres(id) ON DELETE CASCADE
-);
+CREATE TABLE IF NOT EXISTS moviesgenres(
+    movieId int NOT NULL,
+    genreId int NOT NULL,
+    CONSTRAINT fk_moviesgenres_movies FOREIGN KEY (movieId) REFERENCES movies(id) ON DELETE CASCADE,
+    CONSTRAINT fk_moviesgenres_genres FOREIGN KEY (genreId) REFERENCES genres(id) ON DELETE CASCADE
+)Engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS Users(
+CREATE TABLE IF NOT EXISTS users(
     id int AUTO_INCREMENT,
     name varchar(50) NOT NULL,
-    password varchar(50),
-    admin boolean,
+    password varchar(50) NOT NULL,
+    admin boolean NOT NULL,
     CONSTRAINT unq_name UNIQUE (name),
     CONSTRAINT pk_users PRIMARY KEY (id)
-);
+)Engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS Cinema(
+CREATE TABLE IF NOT EXISTS cinemas(
     id int AUTO_INCREMENT,
     name varchar(30) NOT NULL,
     totalCapacity int,
@@ -50,15 +49,15 @@ CREATE TABLE IF NOT EXISTS Cinema(
     enable boolean,
     CONSTRAINT unq_name_address UNIQUE (name, address),
     CONSTRAINT pk_cinemas PRIMARY KEY (id)
-);
+)Engine=InnoDB;
 
-CREATE TABLE IF NOT EXISTS MovieShow(
+CREATE TABLE IF NOT EXISTS movieshow(
     id int AUTO_INCREMENT,
     movieId int,
     cinemaId int,
     showDate date,
     CONSTRAINT pk_shows PRIMARY KEY (id),
-    CONSTRAINT fk_movies FOREIGN KEY (movieId) REFERENCES Movies(id) ON DELETE CASCADE,
-    CONSTRAINT fk_cinema FOREIGN KEY (cinemaId) REFERENCES Cinemas(id) ON DELETE CASCADE,
+    CONSTRAINT fk_movieshow_movies FOREIGN KEY (movieId) REFERENCES movies(id) ON DELETE CASCADE,
+    CONSTRAINT fk_movieshow_cinemas FOREIGN KEY (cinemaId) REFERENCES cinemas(id) ON DELETE CASCADE,
     CONSTRAINT unq_movie_cinema_date UNIQUE (movieId, cinemaId, showDate)
-);
+)Engine=InnoDB;
