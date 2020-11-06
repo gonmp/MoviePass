2e05d0e3c9c65d518352dedd3dd4899f436fb340
Gonzalo Perelstein <gmperelstein@gmail.com>
2020-11-06 18:42:22 -0300

    [Entrega_2] GP: Se aumentó el tamaño de los varchar en el script de la base de datos

--- Scripts/database.sql
+++ Scripts/database.sql
@@ -4,23 +4,23 @@ USE MoviePass;
 
 CREATE TABLE IF NOT EXISTS movies (
     id int,
-    title varchar(30),
+    title varchar(100),
     popularity float,
     vote_count int,
     video boolean,
-    poster_path varchar(50),
+    poster_path varchar(100),
     adult boolean,
-    backdrop_path varchar(50),
-    original_language varchar(50),
-    original_title varchar(30),
+    backdrop_path varchar(100),
+    original_language varchar(100),
+    original_title varchar(100),
     vote_average float,
-    overview varchar(100),
+    overview varchar(500),
     CONSTRAINT pk_movies PRIMARY KEY (id)
 )Engine=InnoDB;
 
 CREATE TABLE IF NOT EXISTS genres(
     id int,
-    name varchar(30) NOT NULL,
+    name varchar(50) NOT NULL,
     CONSTRAINT pk_genres PRIMARY KEY (id)
 )Engine=InnoDB;
 
@@ -42,9 +42,9 @@ CREATE TABLE IF NOT EXISTS users(
 
 CREATE TABLE IF NOT EXISTS cinemas(
     id int AUTO_INCREMENT,
-    name varchar(30) NOT NULL,
+    name varchar(50) NOT NULL,
     totalCapacity int,
-    address varchar(30) NOT NULL,
+    address varchar(50) NOT NULL,
     ticketValue int,
     enable boolean,
     CONSTRAINT unq_name_address UNIQUE (name, address),
