a4c6744ae3a41280de6445f1ecd78e15bbd3cc77
Matias Pascual <matias33pascual@gmail.com>
2020-11-07 22:20:54 -0300

    backup

--- Views/nav-admin-logged.php
+++ Views/nav-admin-logged.php
@@ -7,19 +7,9 @@
     <strong><p class="text-capitalize h4 d-inline text-white float-right">Hello Admin!</p></strong>
 
     <!-- manu del admin -->
-    <div>
-        
-
-    </div>
-
-    <!-- administrar cines -->
-    <a href="<?php echo FRONT_ROOT ?>Home/Logout" class="mx-3 float-right btn btn-outline-danger btn-sm text-white">manage movies</a>
-
-    <!-- administrar peliculas -->
-    <a href="<?php echo FRONT_ROOT ?>Home/Logout" class="mx-3 float-right btn btn-outline-danger btn-sm text-white">manage cinemas</a>
-
-    <!-- administrar funciones -->    
-    <a href="<?php echo FRONT_ROOT ?>Home/Logout" class="mx-3 float-right btn btn-outline-danger btn-sm text-white">manage movies shows</a>
-
-    
+    <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">manage cinemas</a> 
+        <a class="dropdown-item" href="#">manage movies shows</a>
+        <a class="dropdown-item" href="#">manage movies</a>
+    </div>    
 </div>
