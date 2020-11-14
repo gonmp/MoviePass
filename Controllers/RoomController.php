f38640287082008d45f59ac91a9029c55fe831f5
Matias <matias33pascual>
2020-11-14 13:47:02 -0300

    [Movie-30] MP : ya se puede ingresar cualquier horario

---
+++ Controllers/RoomController.php
@@ -0,0 +1,112 @@
+<?php 
+    namespace Controllers;
+
+    use Controllers\HomeController as HomeController;             
+    //use DAO\RoomDAO as RoomDAO;
+    use DAO\CinemaDAO as CinemaDAO;
+
+    class RoomController
+    {        
+        private $roomDAO;
+        private $cinemaDAO;
+        private $cinemaList;
+        
+        public function __construct()
+        {
+            //$roomDAO = new RoomDAO();            
+            $this->cinemaDAO = new CinemaDAO();
+        }        
+
+        public function ShowAddRoom()
+        {   
+            if (HomeController::CheckAdmin() != true) 
+            {
+                HomeController::ForceLogout();
+                return;
+            }
+
+            $this->cinemaList = $this->cinemaDAO->GetALL();
+            require_once(VIEWS_PATH."room-add.php");            
+        }        
+
+        public function Add($cinemaName, $capacity, $ticketValue, $name)
+        {
+            if (HomeController::CheckAdmin() != true) 
+            {
+                HomeController::ForceLogout();
+                return;
+            }                       
+            
+            $cinema = $cinemaDAO->GetCinemaByName($cinemaName);            
+            $room = new Room($capacity, $cinema, $ticketValue, $name);
+
+            $rowAffected = $this->roomDAO->Add($room);
+
+            if ($rowAffected == -1)
+            {                
+                $_SESSION['error'] = 'error in add movie show';
+            }
+            else
+            {
+                $_SESSION['error'] = null;
+            }
+
+            $this->ShowAddRoom();
+        }        
+        
+        public function ShowRoomUpdate($roomId)
+        {
+            if (HomeController::CheckAdmin() != true) 
+            {
+                HomeController::ForceLogout();
+                return;
+            }
+
+            $room = $this->roomDAO->Get($roomId);
+
+            require_once(VIEWS_PATH . 'room-update.php');            
+        }
+
+        public function Update($id, $cinemaName, $ticketValue, $capacity, $name)
+        {
+            if (HomeController::CheckAdmin() != true) 
+            {
+                HomeController::ForceLogout();
+                return;
+            }            
+
+            $cinema = $cinemaDAO->GetCinemaByName($cinemaName);            
+            $room = new Room($capacity, $cinema, $ticketValue, $name);
+            $room->setId($id);            
+
+            $this->movieShowDAO->Update($room);
+
+            $this->ShowCinemaDetails();            
+        }
+
+        public function Delete($roomId)
+        {
+            if (HomeController::CheckAdmin() != true) 
+            {
+                HomeController::ForceLogout();
+                return;
+            }
+
+            $this->movieShowDAO->Delete($roomId);
+            
+            $this->ShowCinemaDetails();   
+        }        
+
+        public function ShowCinemaDetails()
+        {
+            if (HomeController::CheckAdmin() != true) 
+            {
+                HomeController::ForceLogout();
+                return;
+            }
+
+            require_once(VIEWS_PATH."cinema-details.php");
+        }
+    }
+
+?>
--- Models/Room.php
+++ Models/Room.php
@@ -2,27 +2,32 @@
  namespace Models;
 
  class CinemaRoom
- {
-     
+ {     
      private $id;
      private $capacity;
      private $cinema;
      private $ticketValue;
+     private $name;
 
-     public function __construct($capacity, $cinema, $ticketValue){
-         $this->capacity=$capacity;
-         $this->cinema=$cinema;
+     public function __construct($capacity, $cinema, $ticketValue, $name)
+     {
+         $this->capacity = $capacity;
+         $this->cinema = $cinema;
          $this->ticketValue = $ticketValue;
+         $this->name = $name;
      }
      
      public function getId() {return $this->id;}
      public function getCapacity() {return $this->capacity;}
      public function getCinema() {return $this->cinema;}
      public function getTicket() {return $this->ticketValue;}
+     public function getName() {return $this->name;}
+
      
      public function setId($id) {$this->id=$id;}
      public function setCapacity($capacity) {$this->capacity = $capacity;}
      public function setCinema($cinema) {$this->cinema = $cinema;}
      public function setTicket($ticketValue) {$this->ticketValue = $ticketValue;}
+     public function setName($name) {$this->name = $name;}
  }
 ?>
